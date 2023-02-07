efe9ba7f19375b4dd5535e1ea9f2e74ac9cf3c3e
BaldonChristian <baldon.christian@gmail.com>
2023-02-07 17:18:19 +0100

    tolto tutto e messo un alert

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-const div = document.querySelector('.id')
 function click()
 {
     console.log("funziona il click")
-    let div = document.createElement('div')
-    let p = document.createElement('p')
-    let button = document.createElement('button')
-    button.textContent = "chiudi"
-    p.textContent ="Il nuovo film più visto è..."
-
-    button.onclick = function(){
-        body.removeChild(p);
-    }
+    alert("Il miglior film è Batman");
 } 
-
-div.appendChild(p);
-div.appendChild(button)
-body.appendChild(div)
 
